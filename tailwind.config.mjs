331dac69f0c93d94f222099a3ce5d7e9989ad289
thiagofcm <thiagof1503@gmail.com>
2024-07-10 11:55:04 -0300

    first commit with astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
